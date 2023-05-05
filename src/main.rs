1e506b6a6daf299ba3f3a09076935c41671da30e
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2023-05-05 13:36:43 -0300

    add placeholder for listing command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 // Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -9,18 +9,40 @@ struct Cli {
 }
 
 // TODO: implement a fuzzy euristic (https://www.baeldung.com/cs/fuzzy-search-algorithm)
-// TODO: search common local dirs
+// TODO: apt search keyword
+// TODO: apt search .
+// apt search REGEX
+//
+fn generate_fuzzy_alternatives(regex: String) -> Vec<String> {
+    let mut fuzzy_alternatives: Vec<String> = Vec::new();
+    fuzzy_alternatives.push(regex);
+
+    return fuzzy_alternatives;
+}
+
+fn get_all_packages() -> Vec<String> {
+    let mut all_packages = Command::new("apt")
+        .arg("list")
+        .output()
+        .expect("failed to run 'apt list'");
+
+    all_packages = String::from_utf8_lossy(&all_packages.stdout)
+        .split("Listing...\n")
+        .collect()[1]
+}
 
 fn main() {
     let args = Cli::parse();
-    let output = Command::new("apt")
-        .arg("search")
-        .arg("cache")
-        .arg(args.package_name.clone())
-        .output()
-        .expect(format!("failed to search for package {}", args.package_name).as_str());
+    let all_packages = get_all_packages();
+    let regexes = generate_fuzzy_alternatives(args.package_name.clone());
 
-    println!("{}", String::from_utf8(output.stdout).unwrap());
-    //apt-cache search keyword
-    //apt-cache search .
+    for regex in regexes {
+        let output = Command::new("apt")
+            // .args(&["list", regex])
+            .arg("search")
+            .arg(regex)
+            .output()
+            .expect(format!("failed to search for package {}", args.package_name).as_str());
+        println!("{}", String::from_utf8(output.stdout).unwrap());
+    }
 }
